a21286084f736a538777e1ba66a923d1f4e71ccc
Kevin Mougammadaly <kevin.mougammadaly@gmail.com>
2019-11-28 12:34:46 +0100

    add db

--- app.py
+++ app.py
@@ -1,43 +1,71 @@
 from flask import Flask, request, render_template, redirect, url_for
 import csv
+import logging as lg
 
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+
+
+# db variable initialization
+app = Flask(__name__, instance_relative_config=True)
+app.config["DEBUG"] = True
+
+app.config.from_pyfile('config.py')
+db = SQLAlchemy(app)
+db.drop_all()
+db.create_all()
+
+class Test(db.Model):
+    __tablename__ = 'test'
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
+
+
+test = []
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="mouga",
+    password="ipssi2018",
+    hostname="mouga.mysql.pythonanywhere-services.com",
+    databasename="mouga$twitter",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
 @app.route('/')
 def home():
-    return 'Bienvenue !'
+    return 'Bienvenue!'
 
 
 @app.route('/gaz', methods=['GET', 'POST'])
 def save_gazouille():
-	if request.method == 'POST':
-		print(request.form)
-		dump_to_csv(request.form)
-		return redirect(url_for('timeline'))
-		# return "OK"
-	if request.method == 'GET':
-		return render_template('formulaire.html')
+    if request.method == 'POST':
+        print(request.form)
+        dump_to_csv(request.form)
+        return redirect(url_for('timeline'))
+        # return "OK"
+    if request.method == 'GET':
+        return render_template('formulaire.html')
 
 
 @app.route('/timeline', methods=['GET'])
 def timeline():
-	gaz = parse_from_csv()
-	return render_template("timeline.html", gaz=gaz)
+    gaz = parse_from_csv()
+    return render_template("timeline.html", gaz=gaz)
 
 
 def parse_from_csv():
-	gaz = []
-	with open('./gazouilles.csv', 'r') as f:
-		reader = csv.reader(f)
-		for row in reader:
-			gaz.append({"user": row[0], "text": row[1]})
-	return gaz
+    gaz = []
+    with open('./gazouilles.csv', 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            gaz.append({"user": row[0], "text": row[1]})
+    return gaz
 
 
 def dump_to_csv(d):
-	donnees = [d["user-name"], d["user-text"]]
-	with open('./gazouilles.csv', 'a', newline='', encoding='utf-8') as f:
-		writer = csv.writer(f)
-		writer.writerow(donnees)
-
+    donnees = [d["user-name"], d["user-text"]]
+    with open('./gazouilles.csv', 'a', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerow(donnees)
---
+++ instance/config.py
@@ -0,0 +1,8 @@
+import os
+
+DB_USER = 'mouga'
+DB_PASSWORD = 'ipssi2019'
+SQLALCHEMY_DATABASE_URI = 'mouga.mysql.pythonanywhere-services.com'
+DB_NAME = 'twitter'
+
+
